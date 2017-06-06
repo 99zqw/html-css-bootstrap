1704aba83cf990d47e9f4689f85bc0748e1e5ecc
99zqw <2208982655@qq.com>
2017-06-06 20:47:58 +0800

    css bootstrap and html

---
+++ demo.js
@@ -0,0 +1,21 @@
+/**
+ * Created by zhongqianwen on 17-5-21.
+ */
+<html>
+    <body>
+      <h1>统一建模语言理论测试</h1>
+       <p>考试科目:统一建模语言</p>
+        <p>时间：100分钟</p>
+         <p>得分:</p>
+        <form>班级：(必填)<br>   <input type="text"  name="班级"><br>
+              学号：(必填)       <input type="text"  name="学号"><br>
+              姓名：(必填)       <input type="text"  name="姓名"><br>
+         </formm>
+       <p>一、填空题(每空5分，共20分)</p>
+       <form>1.UML的中文全称是：<br>
+        <input  type="text"  name=“UML“
+             2.对象最突出的特征是：<br>
+        <input  type="text"  name="对象"<br>
+        </form>
+
+
